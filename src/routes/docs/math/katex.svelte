6fee540ba6cc1227a12260b4dcc263c58924c807
mattcroat <mattcroat.dev@gmail.com>
2026-02-05 18:43:32 +0100

    feat: add initial Animotion docs with SvelteKit

---
+++ src/routes/docs/math/katex.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Embed as Presentation, Slide, Transition } from '@animotion/core'
+</script>
+
+<Presentation>
+	<Slide class="h-full place-content-center place-items-center">
+		<Transition>
+			<p>
+				The probability of getting {`\\(k\\)`} heads when flipping {`\\(n\\)`} coins
+			</p>
+		</Transition>
+
+		<Transition>
+			{`
+				\\[P(E) = {n \\choose k} p^k (1-p)^{ n-k} \\]
+			`}
+		</Transition>
+	</Slide>
+</Presentation>
