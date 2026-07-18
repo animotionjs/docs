951b6f43e29909e24bb6c7b24d6f28afd4f1a75c
matiadev <mattcroat.dev@gmail.com>
2026-07-18 13:10:30 +0200

    chore: merge Svelte config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import sveltemark from './src/lib/sveltemark/index.js';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			adapter: adapter(),
+			extensions: ['.svelte', '.md'],
+			preprocess: [sveltemark(), vitePreprocess()],
+			compilerOptions: {
+				experimental: {
+					async: true
+				},
+				warningFilter: (warning) => warning.code !== 'a11y_no_noninteractive_tabindex'
+			},
+			experimental: {
+				remoteFunctions: true
+			}
+		})
+	]
 });
